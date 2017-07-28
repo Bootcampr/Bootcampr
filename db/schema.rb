cf48dd799fe65812b960cbf7353a5b4f22ab3421
Kala Wetzler & Preet Dha <pair+kala-lee.preetness@devbootcamp.com>
2017-07-27 17:28:14 -0700

    Finish merge

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20170727232458) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "events", force: :cascade do |t|
     t.string   "title",      null: false
     t.date     "date",       null: false
