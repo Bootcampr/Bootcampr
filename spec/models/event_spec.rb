8d1a0e14d83d412f3fd1eb7b85a5c9ef55cfe476
Kala Wetzler & Preet Dha <pair+kala-lee.preetness@devbootcamp.com>
2017-07-28 15:29:37 -0700

    Complete coverage test for events

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,13 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  let(:test_event) { Event.create(title: 'title', date: '2017-07-27', time: '2000-01-01 23:51:12', location: 'sea', summary: 'that') }
+  
+  context 'validations' do
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_presence_of(:time) }
+    it { is_expected.to validate_presence_of(:location) }
+    it { is_expected.to validate_presence_of(:summary) }
+  end
 
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:date) }
-  it { is_expected.to validate_presence_of(:time) }
-  it { is_expected.to validate_presence_of(:location) }
-  it { is_expected.to validate_presence_of(:summary) }
-  describe 'associations' do
+  it '#format_time' do
+    expect(test_event.format_time).to eq '11:51 PM'
+  end
+
+  it '#formate_date' do
+    expect(test_event.format_date).to eq '07/27/2017'
+  end
+
+  it '#month' do
+    expect(test_event.month).to eq '07'
+  end
+
+  context 'associations' do
     it { is_expected.to belong_to(:owner) }
   end
 end
