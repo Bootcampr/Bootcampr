2ed9b3fde34d2992cba55406190b40f8ba901d38
Kala Wetzler & Preet Dha <pair+kala-lee.preetness@devbootcamp.com>
2017-07-28 09:48:58 -0700

    Display events by month on event#index

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,16 @@ class Event < ApplicationRecord
 
     validates_presence_of :title, :date, :time, :location, :summary
 
+    def format_time
+      self.time.strftime('%I:%M %p')
+    end
+
+    def format_date
+      self.date.strftime('%m/%d/%Y')
+    end
+
+    def month
+      self.format_date[0] + self.format_date[1]
+    end
+
 end
