908d9bf5b3a129ef2d4ebb4ba54fc7dfbea6794c
Jae Easley & Preet Dha <pair+jaesley.preetness@devbootcamp.com>
2017-07-28 14:11:23 -0700

    Begin experimentation for github api calls

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
+require 'rest-client'
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    # p RestClient.get "https://api.github.com/user?client_id=#{ENV['APP_ID']}&client_secret=#{ENV['APP_SECRET']}"
   end
 end
