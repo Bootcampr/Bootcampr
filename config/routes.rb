3ebe40f5bdc70572fa8d1dfe95ea2a3d92345be7
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-29 14:40:45 -0700

    Create button for 'new project' - make title a link to show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events
-  resources :users
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+  resources :users
 
   resources :pages, only: [:index]
 
