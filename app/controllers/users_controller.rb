d81899c4fbae7f4d4a09d1f973a919eef0bc1ba2
Jae Easley <jaesley@gmail.com>
2017-07-29 13:36:31 -0700

    Add categories/tags to models

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :bootcamp, :location, :summary)
+    params.require(:user).permit(:first_name, :last_name, :bootcamp, :location, :summary, :tag_list)
   end
 
 
