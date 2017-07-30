6509b2f59d1032ab93ded181c5431e0b057854a8
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-30 13:34:50 -0700

    Create event cards

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,48 +3,48 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.all.order(:date, :time)
-    @january = []
-    @february = []
-    @march = []
-    @april = []
-    @may = []
-    @june = []
-    @july = []
-    @august = []
-    @september = []
-    @october = []
-    @november = []
-    @december = []
-    # :nocov:
-    @events.each do |e|
-      case e.month
-      when '01'
-        @january << e
-      when '02'
-        @february << e
-      when '03'
-        @march << e
-      when '04'
-        @april << e
-      when '05'
-        @may << e
-      when '06'
-        @june << e
-      when '07'
-        @july << e
-      when '08'
-        @august << e
-      when '09'
-        @september << e
-      when '10'
-        @october << e
-      when '11'
-        @november << e
-      when '12'
-        @december << e
-      end
-    end
-    # :nocov:
+    # @january = []
+    # @february = []
+    # @march = []
+    # @april = []
+    # @may = []
+    # @june = []
+    # @july = []
+    # @august = []
+    # @september = []
+    # @october = []
+    # @november = []
+    # @december = []
+    # # :nocov:
+    # @events.each do |e|
+    #   case e.month
+    #   when '01'
+    #     @january << e
+    #   when '02'
+    #     @february << e
+    #   when '03'
+    #     @march << e
+    #   when '04'
+    #     @april << e
+    #   when '05'
+    #     @may << e
+    #   when '06'
+    #     @june << e
+    #   when '07'
+    #     @july << e
+    #   when '08'
+    #     @august << e
+    #   when '09'
+    #     @september << e
+    #   when '10'
+    #     @october << e
+    #   when '11'
+    #     @november << e
+    #   when '12'
+    #     @december << e
+    #   end
+    # end
+    # # :nocov:
   end
 
   def new
