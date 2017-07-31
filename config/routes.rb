bc66cfbb236a3b4a4f1b90f6ef86df7949616771
Preet Dha <email": null>
2017-07-31 14:20:45 -0700

    Add place holder picture for events

--- config/routes.rb
+++ config/routes.rb
@@ -4,24 +4,18 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: "registrations" }
 
-  resources :events do
-    resources :projects, shallow: true
-  end
-
   resources :pages, only: [:index]
 
   resources :users
 
-  resources :events
   resources :events do
     resources :projects, only: [:new, :create]
   end
 
-  resources :projects
   resources :projects do
     resources :events, only: [:new, :create]
   end
-  
+
   resources :tags, only: [:index, :show]
 
   root to: 'pages#index'
