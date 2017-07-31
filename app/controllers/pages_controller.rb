306b04290e401bc44e777ac92e0e819d708fd1b7
Joshua Rudio <joshuarudio@gmail.com>
2017-07-31 11:25:57 -0700

    Display upcoming events and projects in table form, add table css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @user = User.new
     @events = Event.all
-    @upcoming_events = Event.order(date: :desc).limit(5)
+    @upcoming_events = Event.order(date: :asc).limit(5)
     @recent_projects = Project.order(created_at: :desc).limit(5)
   end
 end
