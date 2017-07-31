e8f01be555079025014c13b238400a58ff407f02
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-31 09:37:43 -0700

    Uncomment tests to pass

--- spec/models/events_project_spec.rb
+++ spec/models/events_project_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EventsProject, type: :model do
   describe '#assocations' do
-    # it { is_expected.to belong_to :event }
-    # it { is_expected.to belong_to :project }
+    it { is_expected.to belong_to :event }
+    it { is_expected.to belong_to :project }
   end
 end
