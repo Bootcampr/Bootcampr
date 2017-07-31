1bb00cb1cd3257ba396f5a30119990b10bd61f2c
Preet Dha <email": null>
2017-07-31 15:59:10 -0700

    Validates repo field with github specific regex; add passing test

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,9 @@ class Project < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   has_many :events_projects
   has_many :events, through: :events_projects
-  
+
   acts_as_taggable
   acts_as_taggable_on :categories
+
+  validates_format_of :repository, :with => /^(http|https):\/\/github.com(([0-9]{1,5})?\/.*)?$/, :multiline => true 
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,13 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+  let!(:test_project) { Project.new(title: 'test', summary: 'test test', repository: 'http://google.com') }
   context '#validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :summary }
+    it 'repository is not valid unless it is a GitHub URL' do
+      expect(test_project).to_not be_valid
+    end
   end
 
   context '#associations' do
     it { is_expected.to belong_to :owner }
-    # it { is_expected.to have_many :events }
   end
+
 end
