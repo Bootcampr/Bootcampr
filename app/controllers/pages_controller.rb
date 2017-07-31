1117dcdb38a6f3f534181d56ad0cc414329ce26e
Joshua Rudio <joshuarudio@gmail.com>
2017-07-30 23:47:13 -0700

    Play around with more things

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @upcoming_events = Event.order(date: :desc).limit(5)
-    @recent_projects = Project.order(created_at: :desc).limit(5)
+    @user = User.new
   end
 end
