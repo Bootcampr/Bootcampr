dd3af8c0ffb4b092f44d2b5d32ed1c2ca4a9c00a
Kala Wetzler <kala.wetzler@gmail.com>
2017-07-31 12:07:47 -0700

    Start to add attendees to show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    resources :attendances, only: [:create, :show]
+  end
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks", registrations: "registrations" }
 
