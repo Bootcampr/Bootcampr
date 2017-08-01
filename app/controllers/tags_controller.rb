da1e2ca325fb3d471456d558ed305194d00adc9c
Jae Easley <jaesley@gmail.com>
2017-08-01 14:11:35 -0700

    Downcase tags for sorting

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = Tag.all.sort_by { |tag| tag.name }
+    @tags = Tag.all.sort_by { |tag| tag.name.downcase }
   end
 
   def show
