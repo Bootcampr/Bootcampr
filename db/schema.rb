116366c707c9829ad8a8ba1ff6dbc727a23773ea
Preet Dha <email": null>
2017-08-01 09:58:43 -0700

    Make repo addresses clickable links

--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,10 @@ ActiveRecord::Schema.define(version: 20170801161817) do
   end
 
   create_table "projects", force: :cascade do |t|
-    t.string   "title"
-    t.text     "summary"
+    t.string   "title",      null: false
+    t.text     "summary",    null: false
     t.text     "stack"
-    t.integer  "owner_id"
+    t.integer  "owner_id",   null: false
     t.string   "repository"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
