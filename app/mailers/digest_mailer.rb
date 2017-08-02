284f5eba0fdd2f43d681b8dc38d6686ee49c06d7
Jae Easley <jaesley@gmail.com>
2017-08-01 17:13:17 -0700

    Reformat email output

--- app/mailers/digest_mailer.rb
+++ app/mailers/digest_mailer.rb
@@ -3,6 +3,7 @@ class DigestMailer < ApplicationMailer
 
   def digest_email(user)
     @user = user
+    attachments.inline["twitter_icon.png"] = File.read("#{Rails.root}/app/assets/images/twitter_icon.png")
     mail(to: @user.email, subject: 'What\'s happening this week on Bootcampr')
   end
 end
