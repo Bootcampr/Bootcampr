0d99fac589b217a944fc5bb9457cbebe41a061dd
Preet Dha <email": null>
2017-08-02 17:37:36 -0700

    Create routes, and use news api to fetch news stories

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,15 @@ class PagesController < ApplicationController
     @upcoming_events = Event.order(date: :asc).limit(5)
     @recent_projects = Project.order(created_at: :desc).limit(5)
   end
+
+  def news
+    request = RestClient.get 'https://newsapi.org/v1/articles?source=google-news&sortBy=top&apiKey=1c1b02c0aa584baab7868f0785dce659'
+    news = JSON.parse(request.body)
+    @articles = []
+    news['articles'].each do |article|
+      @articles << article
+    end
+    render :news_pages
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   get 'users/:id/subscribe', to: 'users#subscriptions', as: :subscribe
   get 'users/:id/unsubscribe', to: 'users#subscriptions', as: :unsubscribe
-
+  get '/news', to: 'pages#news'
   resources :tags, only: [:index, :show]
 
   root to: 'pages#index'
