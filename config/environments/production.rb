d946553a18aa96c09e90afe0c238fdde1c3b39ab
Joshua Rudio <joshuarudio@gmail.com>
2017-08-02 20:20:29 -0700

    Enable caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Used for caching images to client
+  config.action_dispatch.rack_cache = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
